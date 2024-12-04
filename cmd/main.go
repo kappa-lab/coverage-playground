8d14bf9d87246a0783b319cc3acc2f0c8d177e83
gamiken <gamiken@kappa-lab.com>
2024-12-04 16:23:28 +0900

    1st

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package cmd
+
+func main() {}
+
+func hoge(v string) string {
+	if v == "" {
+		return "empty"
+	} else {
+		return v
+	}
+}
---
+++ cmd/main_test.go
@@ -0,0 +1,27 @@
+package cmd
+
+import "testing"
+
+func Test_hoge(t *testing.T) {
+	type args struct {
+		v string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "empty",
+			args: args{v: ""},
+			want: "empty",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hoge(tt.args.v); got != tt.want {
+				t.Errorf("hoge() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
